fe5d0f1849585dd74d848872a132256a89142457
Asyl <asylbek1476@gmail.com>
2023-11-03 01:39:52 +0600

    added py

---
+++ object-model.py
@@ -0,0 +1,29 @@
+class Diary:
+    def __init__(self, student):
+        self.student = student
+        self.subjects = []
+
+    def add_subject(self, subject):
+        self.subjects.append(subject)
+
+class Subject:
+    def __init__(self, name):
+        self.name = name
+
+class Student:
+    def __init__(self, name):
+        self.name = name
+
+
+student1 = Student("Adil")
+math_subject = Subject("Math")
+russian_subject = Subject("russ!!!")
+diary = Diary(student1)
+
+diary.add_subject(math_subject)
+diary.add_subject(russian_subject)
+
+print(f"Student: {diary.student.name}")
+print("Subjects:")
+for subject in diary.subjects:
+    print(f"- {subject.name}")
